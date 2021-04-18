35d3c8c59a1c96c337d2ee091a04e1ec57484b7e
Lucas Medeiros <freitasmlucas@gmail.com>
2021-04-18 09:50:57 -0300

    adding scheduler example

---
+++ scheduler/config/settings.js
@@ -0,0 +1,9 @@
+const env = require("env-var");
+
+const settings = {
+  NODE_ENV: env.get("NODE_ENV").required().asString(),
+  commitMessageUrl: env.get("APICommitMessagesURL").required().asUrlString(),
+  dbTableName: env.get("DbTableName").required().asString(),
+};
+
+module.exports = settings;
